5dffae2da3ad3556fb4d5e8841db73ca2e8ffa26
bullyto <apero.nuit.du.66@gmail.com>
2026-06-05 01:05:06 +0200

    Add files via upload

--- apps/stock/sw.js
+++ apps/stock/sw.js
@@ -1,5 +1,5 @@
-const CACHE='adn66-stock-5tabs-v1';
-const FILES=['./','./index.html','./manifest.webmanifest','./assets/camera-beta.svg'];
+const CACHE='adn66-stock-mobile-json-v1';
+const FILES=['./','./index.html','./manifest.webmanifest','./assets/placeholder.svg','./assets/camera-beta.svg','./stock/_snapshot.json','./stock/_snapshot_local.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
