ac30fce1c040b85b15684241c10a8e325801c9be
bullyto <apero.nuit.du.66@gmail.com>
2026-06-05 00:53:36 +0200

    Add files via upload

--- apps/stock/sw.js
+++ apps/stock/sw.js
@@ -1,18 +1,5 @@
-const CACHE_NAME = 'adn66-stock-refonte-v1';
-const CORE = ['./','./index.html','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(CORE)).catch(()=>{}));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-    const copy = res.clone();
-    caches.open(CACHE_NAME).then(c => c.put(e.request, copy)).catch(()=>{});
-    return res;
-  }).catch(() => caches.match('./index.html'))));
-});
+const CACHE='adn66-stock-5tabs-v1';
+const FILES=['./','./index.html','./manifest.webmanifest','./assets/camera-beta.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
